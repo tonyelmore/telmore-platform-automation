3a01a379d8d7f2050e8d18c64182e1c50f90d33b
Tony Elmore <aelmore@pivotal.io>
2020-06-26 15:12:36 -0500

    add opsman cert as trusted

--- environments/config/create-cert.sh
+++ environments/config/create-cert.sh
@@ -1,15 +1,19 @@
+
 credhub generate -n concourse/main/pas_network_cert -t certificate \
-  -c *.sys.sbx.my3votes.com \
-  -a *.sys.sbx.my3votes.com \
-  -a *.apps.sbx.my3votes.com \
-  -a *.login.sys.sbx.my3votes.com \
-  -a *.uaa.sys.sbx.my3votes.com \
+  -c "*.sys.sbx.my3votes.com" \
+  -a "*.sys.sbx.my3votes.com" \
+  -a "*.apps.sbx.my3votes.com" \
+  -a "*.login.sys.sbx.my3votes.com" \
+  -a "*.uaa.sys.sbx.my3votes.com" \
   --self-sign
 
 
 credhub generate -n concourse/main/uaa_service_provider_key_credentials -t certificate \
-  -c *.login.sys.sbx.viewlifeline.com \
-  -a *.login.sys.sbx.viewlifeline.com \
+  -c "*.login.sys.sbx.my3votes.com" \
+  -a "*.login.sys.sbx.my3votes.com" \
   --self-sign
 
 
+
+# Network -> *.app.sbx.my3votes.com, *.sys.sbx.my3votes.com, *.login.sys.sbx.my3votes.com, *.uaa.sys.sbx.my3votes.com
+# UAA -> *.login.sys.sbx.my3votes.com
