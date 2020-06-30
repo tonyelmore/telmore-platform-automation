fb2d6a9264036414fdafff5ad111797cabb03de7
Tony Elmore <aelmore@pivotal.io>
2020-06-29 21:47:02 -0600

    trusted cert

--- environments/config/create-cert.sh
+++ environments/config/create-cert.sh
@@ -14,6 +14,8 @@ credhub generate -n concourse/main/uaa_service_provider_key_credentials -t certi
   --self-sign
 
 
+# To get the CA to add as "trusted certs" in director configuration (security tab)
+# credhub get -n concourse/main/pas_network_cert -k ca 
 
 # Network -> *.app.sbx.my3votes.com, *.sys.sbx.my3votes.com, *.login.sys.sbx.my3votes.com, *.uaa.sys.sbx.my3votes.com
 # UAA -> *.login.sys.sbx.my3votes.com
