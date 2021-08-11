897048d34a6f6abf9fa6ba90fca135c040f7599e
Tony Elmore <tony@tonyelmore.com>
2021-08-11 15:36:42 -0700

    More testing of certs

--- environments/scripts/create-cert.sh
+++ environments/scripts/create-cert.sh
@@ -14,6 +14,14 @@ credhub generate -n concourse/main/uaa_service_provider_key_credentials -t certi
   --self-sign
 
 
+credhub generate -n concourse/main/pks-tls-cert -t certificate \
+  -c "*.pks.my3votes.com" \
+  -a "*.pks.my3votes.com" \
+  -a "*.pks.sys.sbx.my3votes.com" \
+  -a "*.pks.sbx.my3votes.com" \
+  --self-sign
+
+
 # To get the CA to add as "trusted certs" in director configuration (security tab)
 # credhub get -n concourse/main/pas_network_cert -k ca 
 # Or could add pas_network_cert.ca to "trusted-certs" in the director config  (I think it is .ca)
